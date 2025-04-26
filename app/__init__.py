bb25185e7f4dcaffd0569dc1d444dbb52f9a8670
daviruasfb@gmail.com <daviruasfb@gmail.com>
2025-04-26 17:29:04 -0300

    Fixing path to templates

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,10 @@ import os
 load_dotenv()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+        template_folder=os.path.abspath("templates"),  
+        static_folder=os.path.abspath("static")       
+    )
     
     from .routes import bp
     app.register_blueprint(bp)
